75eee692c8006122f4d3f2225c31438ec1b2896f
Erich Jagomägis <Erich.Jagomagis@partner.smit.ee>
2018-02-27 16:25:45 +0200

    Fixed Krediidipank url-s to accomodate Cooppank domain name. Increased version to 3.0.2

--- src/main/java/ee/bitweb/banklink/sdk/banks/krediidipank/Krediidipank.java
+++ src/main/java/ee/bitweb/banklink/sdk/banks/krediidipank/Krediidipank.java
@@ -14,21 +14,21 @@ public class Krediidipank extends Banklink {
 
     private static final String BANKID = "KREP";
 
-    protected String authenticationRequestUri = "https://i-pank.krediidipank.ee/auth";
-    protected String testAuthenticationRequestUri = "https://secure.krediidipank.ee/auth";
+    protected String authenticationRequestUri = "https://i-pank.cooppank.ee/auth";
+    protected String testAuthenticationRequestUri = "https://secure.cooppank.ee/auth";
 
 
     public Krediidipank(Protocol protocol) {
         super(protocol);
-        super.requestUri = "https://i-pank.krediidipank.ee/pay";
-        super.testRequestUri = "https://secure.krediidipank.ee/pay";
+        super.requestUri = "https://i-pank.cooppank.ee/pay";
+        super.testRequestUri = "https://secure.cooppank.ee/pay";
         super.fields = new Fields();
     }
 
     public Krediidipank(Protocol protocol, String encoding, String language, String currency) {
         super(protocol, encoding, language, currency);
-        super.requestUri = "https://i-pank.krediidipank.ee/pay";
-        super.testRequestUri = "https://secure.krediidipank.ee/pay";
+        super.requestUri = "https://i-pank.cooppank.ee/pay";
+        super.testRequestUri = "https://secure.cooppank.ee/pay";
         super.fields = new Fields();
     }
 
